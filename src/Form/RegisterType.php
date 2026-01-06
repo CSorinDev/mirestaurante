08e3082ca1401342b5fb7bb74a86ab08cbc71e5a
Sorin Andrei Craciun <csorin.dev@gmail.com>
2026-01-06 12:45:41 +0100

    changed form validations

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -7,50 +7,46 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $firstLetter           = "/^[a-zA-Z]/";
-        $minLength6            = "/.{6,}/";
-        $doubleSpaceNotAllowed = "/^(?!.* {2,}).*$/";
-        $lastChar              = "/[a-zA-Z0-9]$/";
-        $emailFormat           = "/.{6,}@.{3,}\..{2,3}/";
-        $phoneFirstDigit       = "/^[6-7]/";
-        $has9Digits            = "/^[0-9]{9}$/";
-
         $builder
             ->add('username', TextType::class, [
                 "label"       => "Usuario",
                 "attr"        => ["autofocus" => "focus"],
                 "constraints" => [
-                    new Regex($firstLetter, "Debe empezar por una letra"),
-                    new Regex($minLength6, "Debe tener mínimo 6 caracteres"),
-                    new Regex($lastChar, "Debe acabar en una letra o un dígito"),
+                    new NotBlank(message: "El usuario es obligatorio"),
+                    new Length(min: 6, minMessage: "El usuario debe tener mínimo 6 caracteres"),
+                    new Regex(pattern: "/[a-zA-Z0-9]/$", message: "El usuario debe acabar en una letra o en un dígito"),
                 ],
             ])
             ->add("name", TextType::class, [
                 "label"       => "Nombre y Apellidos",
                 "constraints" => [
-                    new Regex($firstLetter, "Debe empezar por una letra"),
-                    new Regex($minLength6, "Debe tener mínimo 6 caracteres"),
-                    new Regex($doubleSpaceNotAllowed, "No puede tener 2 espacios seguidos"),
+                    new NotBlank(message: "El nombre completo es obligatorio"),
+                    new Length(min: 6, minMessage: "El nombre completo debe tener al menos 6 caracteres"),
                 ],
             ])
             ->add("email", EmailType::class, [
                 "label"       => "Correo",
                 "constraints" => [
-                    new Regex($firstLetter, "Debe empezar por una letra"),
-                    new Regex($emailFormat, "Formato de email incorrecto"),
+                    new NotBlank(message: "El email es obligatorio"),
+                    new Regex(
+                        pattern: "/^[a-zA-Z].{5,}@[a-zA-Z0-9]{3,}\.[a-zA-Z]{2-3}$/",
+                        message: "El formato del correo no es correcto"
+                    ),
                 ],
             ])
             ->add("phone", TelType::class, [
                 "label"       => "Teléfono",
                 "constraints" => [
-                    new Regex($phoneFirstDigit, "Debe empezar por 6 o por 7"),
-                    new Regex($has9Digits, "Debe tener 9 dígitos"),
+                    new NotBlank(message: "El número de teléfono es obligatorio"),
+                    new Regex(pattern: "/^[6-7][0-9]{8}$/", message: "Número de teléfono móvil inválido"),
                 ],
             ])
             ->add("submit_button", SubmitType::class, [
