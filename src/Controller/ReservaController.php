c7676a86af58f939ca3a4961ca7c75ccf10fc7a4
Sorin Andrei Craciun <csorin.dev@gmail.com>
2026-02-13 18:49:39 +0100

    done reserva

--- src/Controller/ReservaController.php
+++ src/Controller/ReservaController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Reserva;
 use App\Form\ReservaType;
+use App\Repository\ReservaRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,32 +15,47 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ReservaController extends AbstractController
 {
     #[Route('/reserva', name: 'app_reserva')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em, ReservaRepository $reservaRepository): Response
     {
-        // Verificar que el usuario está autenticado
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        // $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         
         $reserva = new Reserva();
         $form = $this->createForm(ReservaType::class, $reserva);
         $form->handleRequest($request);
 
-        // Solo guardar si el formulario fue enviado Y es válido
+        $reservas = $reservaRepository->findBy(
+            ['user' => $this->getUser()],
+            ['fecha' => "desc", "hora" => "desc"]
+        );
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // Asignar el usuario actual a la reserva
             $reserva->setUser($this->getUser());
             
             $em->persist($reserva);
             $em->flush();
 
-            // Mensaje de éxito
             $this->addFlash('success', '¡Reserva realizada con éxito!');
 
-            // Redirigir para evitar reenvío del formulario
             return $this->redirectToRoute('app_my_profile');
         }
 
         return $this->render('reserva/index.html.twig', [
             'form' => $form,
+            'reservas' => $reservas
+        ]);
+    }
+
+    #[Route(path: "/misreservas", name: "app_mis_reservas")]
+    public function misReservas(ReservaRepository $reservaRepository) {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $reservas = $reservaRepository->findBy(
+            ['user' => $this->getUser()],
+            ['fecha' => "desc", "hora" => "desc"]
+        );
+
+        return $this->render('reserva/mis_reservas.html.twig',[
+            'reservas' => $reservas
         ]);
     }
 }
