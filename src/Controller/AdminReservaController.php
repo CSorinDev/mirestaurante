b7a306728a404eae217c7d77d4ac25e259422690
Sorin Andrei Craciun <csorin.dev@gmail.com>
2026-02-16 16:22:22 +0100

    done links from /admin

--- src/Controller/AdminReservaController.php
+++ src/Controller/AdminReservaController.php
@@ -1,11 +1,8 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Reserva;
 use App\Repository\ReservaRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
