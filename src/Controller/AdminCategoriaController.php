c7a7ec54cbd2941f14a1ba2db2d8f3943fdc9bff
Sorin Andrei Craciun <csorin.dev@gmail.com>
2026-02-26 11:52:54 +0100

    fix: fixed category delete

--- src/Controller/AdminCategoriaController.php
+++ src/Controller/AdminCategoriaController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Categoria;
@@ -11,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(path:'/admin/categoria')]
+#[Route(path: '/admin/categoria')]
 final class AdminCategoriaController extends AbstractController
 {
     #[Route(name: 'app_admin_categoria_index', methods: ['GET'])]
@@ -23,37 +22,37 @@ final class AdminCategoriaController extends AbstractController
     }
 
     #[Route('/new', name: 'app_admin_categoria_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new (Request $request, EntityManagerInterface $entityManager): Response
     {
-        $categorium = new Categoria();
-        $form = $this->createForm(CategoriaType::class, $categorium);
+        $categoria = new Categoria();
+        $form      = $this->createForm(CategoriaType::class, $categoria);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($categorium);
+            $entityManager->persist($categoria);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_admin_categoria_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('admin_categoria/new.html.twig', [
-            'categorium' => $categorium,
-            'form' => $form,
+            'categoria' => $categoria,
+            'form'      => $form,
         ]);
     }
 
     #[Route('/{id}', name: 'app_admin_categoria_show', methods: ['GET'])]
-    public function show(Categoria $categorium): Response
+    public function show(Categoria $categoria): Response
     {
         return $this->render('admin_categoria/show.html.twig', [
-            'categorium' => $categorium,
+            'categoria' => $categoria,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_admin_categoria_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Categoria $categorium, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Categoria $categoria, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(CategoriaType::class, $categorium);
+        $form = $this->createForm(CategoriaType::class, $categoria);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -63,19 +62,24 @@ final class AdminCategoriaController extends AbstractController
         }
 
         return $this->render('admin_categoria/edit.html.twig', [
-            'categorium' => $categorium,
-            'form' => $form,
+            'categoria' => $categoria,
+            'form'      => $form,
         ]);
     }
 
     #[Route('/{id}', name: 'app_admin_categoria_delete', methods: ['POST'])]
-    public function delete(Request $request, Categoria $categorium, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, Categoria $categoria, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$categorium->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($categorium);
+        if ($this->isCsrfTokenValid('delete' . $categoria->getId(), $request->getPayload()->getString('_token'))) {
+            if ($categoria->getCartaItems()->count() > 0) {
+                $this->addFlash('error', 'No se puede eliminar la categoría porque tiene platos asociados.');
+                return $this->redirectToRoute('app_admin_categoria_index', []);
+            }
+            $this->addFlash('success', 'Categoría eliminada correctamente.');
+            $entityManager->remove($categoria);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_admin_categoria_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_admin_categoria_index', []);
     }
 }
