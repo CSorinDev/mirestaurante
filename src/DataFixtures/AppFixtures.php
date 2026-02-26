55580b6a58234cdcb12ed1a0416d4fd7826048b2
Sorin Andrei Craciun <csorin.dev@gmail.com>
2026-02-26 08:32:35 +0100

    feature - added some fixtures reservations

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Carta;
 use App\Entity\Categoria;
+use App\Entity\Reserva;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -19,7 +20,7 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // 1. Crear el usuario Administrador (para que no te quedes fuera)
+        // 1. Crear el usuario Administrador
         $user = new User();
         $user->setEmail('csorin.dev@gmail.com');
         $user->setName('Sorin');
@@ -27,7 +28,34 @@ class AppFixtures extends Fixture
         $user->setIsVerified(true);
         $user->setRoles(['ROLE_ADMIN']);
         $user->setPassword($this->hasher->hashPassword($user, 'parola'));
+        
+        // Crear un usuario con role_user
+        $user2 = new User();
+        $user2->setEmail('ejemplo@gmail.com');
+        $user2->setName('Ejemplo');
+        $user2->setPhone('123456789');
+        $user2->setIsVerified(true);
+        $user2->setRoles(['ROLE_USER']);
+        $user2->setPassword($this->hasher->hashPassword($user2, 'contraseña'));
+
+        $manager->persist($user2);
         $manager->persist($user);
+
+        // Crear reservas de ejemplo
+        $reserva1 = new Reserva();
+        $reserva1->setUser($user);
+        $reserva1->setFecha(new \DateTime('2026-03-04'));
+        $reserva1->setHora(new \DateTime('19:00'));
+        $reserva1->setComensales(4);
+
+        $reserva2 = new Reserva();
+        $reserva2->setUser($user2);
+        $reserva2->setFecha(new \DateTime('2026-03-04'));
+        $reserva2->setHora(new \DateTime('19:00'));
+        $reserva2->setComensales(2);
+
+        $manager->persist($reserva1);
+        $manager->persist($reserva2);
 
         // 2. Crear Categorías y guardarlas en un array para usarlas luego
         $categoriasData = [
