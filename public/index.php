d4e9a4dd53b5521a5d951a4e1a8d2408287e8fc9
Sorin Andrei Craciun <csorin.dev@gmail.com>
2026-02-26 10:05:57 +0100

    index like before and added pan-de-ajo-image

--- public/index.php
+++ public/index.php
@@ -2,12 +2,6 @@
 
 use App\Kernel;
 
-// 1. Añade esta línea para desactivar la búsqueda del archivo .env
-$_SERVER['APP_RUNTIME_OPTIONS'] = [
-    'disable_dotenv' => true,
-];
-
-// 2. El resto de tu código se mantiene igual
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
